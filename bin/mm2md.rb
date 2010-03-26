2cc1f9f9841167a9a5f6accdca50313de43b1727
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2010-03-26 14:26:36 +0100

    more refactoring

--- bin/mm2md.rb
+++ bin/mm2md.rb
@@ -2,19 +2,35 @@ require "rubygems"
 
 input = IO.readlines('../wiki/Installation.mm','').to_s
 
-transformations = [
+markdown = [
     # headlines
     {
       :regexp => /=+\s*?$/,
       :replacement => ''
     },
+    {
+      :regexp => /^=+/,
+      :replacement => lambda {|match| "#" * match.length}
+    },
 
-    # { :regexp => /^(?:(\{\{\{)|(\}\}\}))\s*?$/,:replacement => '....' },    # blocks (extra line)
 
     # blocks (inline code)
     {
       :regexp => /\{\{\{(.*?)\}\}\}/,
       :replacement => '`\1`'
+    },
+    
+    # doing blocks/code
+    {
+      :regexp => /\{\{\{.*?\}\}\}/m,
+      :replacement => lambda do |match|
+        block = match.split("\n")
+        block.shift
+        block.pop
+        "\n" << block.each.inject("") do |acc, line|
+          acc << "    " << line << "\n"
+        end
+      end
     },
 
     # bold
@@ -35,59 +51,48 @@ transformations = [
       :replacement => '_\_\1'
     },
 
+    # processing tables
+    {
+      :regexp => /^\s(\|\|.*?)^\s([^\|])/m,
+      :replacement => lambda do |match|
+        table = $1
+        rest = $2 # this a bit ugly, but it works
+
+        table.gsub!(/&/, '&amp;')
+        # ruby does not support look behind! :(
+        # table.gsub!(/(?!\|\|)</, '&lt;')
+        # table.gsub!(/(?!\|\|<-\d)>/, '&gt;')
+
+        table.gsub!(/^\|\|<\|(\d+)>/, '  <tr><td rowspan="\1">')
+        table.gsub!(/^\|\|<-(\d+)>/, '  <tr><td colspan="\1">')
+        table.gsub!(/^\|\|/, "<tr><td>")
+
+        table.gsub!(/\|\|$/, "</td></tr>")
+
+        table.gsub!(/\|\|<\|(\d+)>/, '<td rowspan="\1">')
+        table.gsub!(/\|\|<-(\d+)>/, '<td colspan="\1">')
+        table.gsub!(/\|\|/, "</td><td>")
+
+        # undo some markdown syntax, since they are not supported in block-elements like tables
+        table.gsub!(/\*\*([^\*]+)\*\*/, '<strong>\1</strong>')
+        table.gsub!(/\*([^\*]+)\*/, '<em>\1</em>')
+
+        "<table>" << "\n" << table << "</table>" << "\n\n" << rest
+      end
+    },
+
     # comment out some unsupported stuff
     # TOC
     {
       :regexp => /(<<TableOfContents\(.\)>>)/,
       :replacement => '<!-- \1 -->'
     }
-  ]
-
-# do the basic stuff
-output = transformations.inject(input) do |txt, pattern|
-  txt.gsub(pattern[:regexp], pattern[:replacement])
+  ].inject(input) do |txt, pattern|
+    if pattern[:replacement].respond_to?(:call)
+      txt.gsub(pattern[:regexp], &pattern[:replacement])
+    else
+      txt.gsub(pattern[:regexp], pattern[:replacement])
+    end
 end
 
-# transform headlines
-output.gsub!(/^=+/) do |match|
-  "#" * match.length
-end
-
-# doing blocks/code
-output.gsub!(/\{\{\{.*?\}\}\}/m) do |match|
-  block = match.split("\n")
-  block.shift
-  block.pop
-  "\n" << block.each.inject("") do |acc, line|
-    acc << "    " << line << "\n"
-  end
-end
-
-# do the tables!
-output.gsub!(/^\s(\|\|.*?)^\s([^\|])/m) do |match|
-  table = $1
-  rest = $2
-
-  table.gsub!(/&/, '&amp;')
-  # ruby does not support look behind! :(
-  # table.gsub!(/(?!\|\|)</, '&lt;')
-  # table.gsub!(/(?!\|\|<-\d)>/, '&gt;')
-
-  table.gsub!(/^\|\|<\|(\d+)>/, '<tr><td rowspan="\1">')
-  table.gsub!(/^\|\|<-(\d+)>/, '<tr><td colspan="\1">')
-  table.gsub!(/^\|\|/, "<tr><td>")
-            
-  table.gsub!(/\|\|$/, "</td></tr>")
-            
-  table.gsub!(/\|\|<\|(\d+)>/, '<td rowspan="\1">')
-  table.gsub!(/\|\|<-(\d+)>/, '<td colspan="\1">')
-  table.gsub!(/\|\|/, "</td><td>")
-
-  # undo some markdown syntax, since they are not supported in block-elements like tables
-  table.gsub!(/\*\*([^\*]+)\*\*/, '<strong>\1</strong>')
-  table.gsub!(/\*([^\*]+)\*/, '<em>\1</em>')
-
-  "<table>" << "\n" << table << "</table>" << "\n\n" << rest
-end
-
-puts output
+puts markdown
