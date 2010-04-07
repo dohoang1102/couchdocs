4667b56513924ee5bab4a0355b3ec37f45301b3f
Paul J. Davis <paul.joseph.davis@gmail.com>
2010-04-07 17:29:29 -0400

    Support unicode in header values.

--- bin/buildweb.py
+++ bin/buildweb.py
@@ -18,6 +18,9 @@ from jinja2.utils import open_if_exists
 
 __usage__ = "usage: %prog [OPTIONS]"
 
+def utf8(val):
+    return unicode(val, "utf-8")
+
 class Config(dict):
     def __getattr__(self, name):
         return self.__getitem__(name)
@@ -44,7 +47,8 @@ class Site(object):
             tgt_path = curr_path.replace(INPUT_PATH, OUTPUT_PATH)
             if not os.path.isdir(tgt_path):
                 os.makedirs(tgt_path)
-            self.process(files, curr_path, tgt_path)
+            files = map(utf8, files)
+            self.process(files, utf8(curr_path), utf8(tgt_path))
 
     def filter(self, names):
         i = 0
@@ -85,20 +89,20 @@ class Page(object):
         self.body = ""
 
         with open(self.source, 'Ur') as handle:
-            raw = handle.read()
+            raw = handle.read().decode("utf-8")
         
         try:
-            headers, body = raw.split("\n\n", 1)
+            headers, body = raw.split(u"\n\n", 1)
         except ValueError:
-            headers, body = "", raw
+            headers, body = u"", raw
 
         try:
             for line in headers.splitlines():
-                name, value = line.split(':', 1)
+                name, value = line.split(u':', 1)
                 self.headers[name.strip()] = value.strip()
         except ValueError:
             self.headers = {}
-            body = "\n\n".join([headers, body])
+            body = u"\n\n".join([headers, body])
         self.headers['pubDate'] = ctime = os.stat(self.source).st_ctime
         self.headers['published'] = datetime.datetime.fromtimestamp(ctime)
 
@@ -107,12 +111,12 @@ class Page(object):
         converter = getattr(self, "convert_%s" % oldext, lambda x: x)
         self.body = converter(body)
 
-        newext = self.headers.get('ext', '.html')
-        self.target = os.path.join(tgt_path, "%s%s" % (basename, newext))
+        newext = self.headers.get('ext', u'.html')
+        self.target = os.path.join(tgt_path, u"%s%s" % (basename, newext))
                 
     def url(self):
         path = self.target.split(self.site.cfg.OUTPUT_PATH)[1].lstrip('/')
-        return "/".join([self.site.url, path])
+        return u"/".join([self.site.url, path])
 
     def needed(self):
         for f in "force --force -f":
@@ -151,7 +155,7 @@ class Page(object):
 
     def convert_md(self, body):
         from markdown import markdown
-        return markdown(body.decode("utf-8"))
+        return markdown(body)
 
     def convert_rst(self, body):
         from docutils.core import publish_parts
