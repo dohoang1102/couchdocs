705520d653f790e42e793179502eb2def230e278
Paul J. Davis <paul.joseph.davis@gmail.com>
2010-04-07 17:35:23 -0400

    Added wikilinks extension to markdown processing

--- bin/buildweb.py
+++ bin/buildweb.py
@@ -155,7 +155,7 @@ class Page(object):
 
     def convert_md(self, body):
         from markdown import markdown
-        return markdown(body)
+        return markdown(body, ['wikilinks'])
 
     def convert_rst(self, body):
         from docutils.core import publish_parts
